b2550bfc79a98a5bcd4f37f3247f223e43d0a869
M Nur Fauzan W <fauzan.widyanto1@gmail.com>
2018-11-27 21:58:30 +0700

    improving design

--- application/views/template/_footer.php
+++ application/views/template/_footer.php
@@ -1,9 +1,8 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 1.0.0
+      Simplify your business
     </div>
-    <strong>Virgorasion &copy; 2018 SMKN 2 Surabaya.</strong> All rights
-    reserved.
+    <strong>Virgorasion &copy; 2018 SMKN 2 Surabaya.</strong>
   </footer>
   <div class="control-sidebar-bg"></div>
 </div>
