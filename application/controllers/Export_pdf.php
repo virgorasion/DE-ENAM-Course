e061fc03bb19192999c343f75b2f1b9348e569c9
Virgorasion <fauzan.widyanto1@gmail.com>
2019-01-16 09:22:55 -0800

    fix some data for export pdf AKB

--- application/controllers/Export_pdf.php
+++ application/controllers/Export_pdf.php
@@ -15,8 +15,10 @@ class Export_pdf extends CI_controller
 
     function PDF_small($kodeInstansi = "010.03",$kodeProgram = "127.01",$kodeKegiatan = "080.01")
     {
-        $siswa = $this->db->query("SELECT nisn,nis,nama FROM tb_siswa WHERE kode_instansi = $kodeInstansi AND kode_program = $kodeProgram")->result();
-        $
+        $data_siswa = $this->db->query("SELECT nisn,nis,nama,jurusan,nomor_hp FROM tb_siswa WHERE kode_instansi = $kodeInstansi AND kode_program = $kodeProgram")->result();
+        $data_sekolah = $this->db->query("SELECT nama_instansi FROM tb_instansi WHERE kode_instansi = $kodeInstansi")->result();
+        $data_program = $this->db->query("SELECT nama_program FROM tb_program WHERE kode_instansi = $kodeInstansi AND kode_program = $kodeProgram")->result();
+        
 
         $pdf = new FPDF();
         $pdf->AddPage('L','A4');
@@ -46,8 +48,8 @@ class Export_pdf extends CI_controller
         $pdf->Cell(20,10,"PROGRAM");
         $pdf->SetX(80);
         $pdf->Cell(5,10," : ",0,0,"C");
-        $pdf->Cell(30,10,"(11739)",0,0,"L");
-        $pdf->Cell(30,10,"Peningkatan Kekuatan Super");
+        $pdf->Cell(20,10,"(".$kodeProgram.")" ,0,0,"L");
+        $pdf->Cell(30,10, @$data_program[0]->nama_program);
         $pdf->Ln(5);
         //KEGIATAN
         $pdf->SetX(30);
@@ -85,43 +87,41 @@ class Export_pdf extends CI_controller
         $pdf->SetX(30);
         $pdf->Cell(20,10,"PENGGUNA ANGGARAN");
         $pdf->SetX(80);
-        $pdf->Cell(5,10," : ",0,0,"C");
-        $pdf->Cell(30,10,"........................");
         $pdf->Ln(5);
         //NAMA SISWA
         $pdf->SetX(30);
         $pdf->Cell(20,10,"NAMA SISWA");
         $pdf->SetX(80);
         $pdf->Cell(5,10," : ",0,0,"C");
-        $pdf->Cell(30,10,"........................");
+        $pdf->Cell(30,10, @$data_siswa[0]->nama);
         $pdf->Ln(5);
         //NO REG SIWA
         $pdf->SetX(30);
         $pdf->Cell(20,10,"NO REG SIWA");
         $pdf->SetX(80);
         $pdf->Cell(5,10," : ",0,0,"C");
-        $pdf->Cell(30,10,"........................");
+        $pdf->Cell(30,10, @$data_siswa[0]->nisn);
         $pdf->Ln(5);
         //SEKOLAH
         $pdf->SetX(30);
         $pdf->Cell(20,10,"SEKOLAH");
         $pdf->SetX(80);
         $pdf->Cell(5,10," : ",0,0,"C");
-        $pdf->Cell(30,10,"........................");
+        $pdf->Cell(30,10, @$data_sekolah[0]->nama_instansi);
         $pdf->Ln(5);
         //JURUSAN
         $pdf->SetX(30);
         $pdf->Cell(20,10,"JURUSAN");
         $pdf->SetX(80);
         $pdf->Cell(5,10," : ",0,0,"C");
-        $pdf->Cell(30,10,"........................");
+        $pdf->Cell(30,10, @$data_siswa[0]->jurusan);
         $pdf->Ln(5);
         //NO HP
         $pdf->SetX(30);
         $pdf->Cell(20,10,"NO HP");
         $pdf->SetX(80);
         $pdf->Cell(5,10," : ",0,0,"C");
-        $pdf->Cell(30,10,"........................");
+        $pdf->Cell(30,10, @$data_siswa[0]->nomor_hp);
         $pdf->Ln(5);
 
         //New Page
@@ -140,21 +140,21 @@ class Export_pdf extends CI_controller
         $pdf->SetX(20);
         $pdf->Cell(40,10,"Nama Siswa");
         $pdf->Cell(5,10,":");
-        $pdf->Cell(30,10,"......................");
+        $pdf->Cell(30,10, @$data_siswa[0]->nama);
         $pdf->Ln(5);
         //PROGRAM
         $pdf->SetFont("Arial","",10);
         $pdf->SetX(20);
         $pdf->Cell(40,10,"PROGRAM");
         $pdf->Cell(5,10,":");
-        $pdf->Cell(30,10,"......................");
+        $pdf->Cell(30,10, @$data_program[0]->nama_program);
         $pdf->Ln(5);
         //SEKOLAH
         $pdf->SetFont("Arial","",10);
         $pdf->SetX(20);
         $pdf->Cell(40,10,"SEKOLAH");
         $pdf->Cell(5,10,":");
-        $pdf->Cell(30,10,"......................");
+        $pdf->Cell(30,10, @$data_sekolah[0]->nama_instansi);
         $pdf->Ln();
 
         //Data Table
@@ -175,12 +175,13 @@ class Export_pdf extends CI_controller
         $pdf->Cell(30,5,'Rp',1,0,"C");
         $pdf->Ln(5);
         //Data
-        $data_kegiatan = $this->db->query("SELECT tb_kegiatan.kode_kegiatan,tb_kegiatan.nama_kegiatan,tb_kegiatan.total_rinci,
-            (SELECT SUM(tb_rekening.triwulan_1) FROM tb_rekening WHERE tb_rekening.kode_instansi = $kodeInstansi AND tb_rekening.kode_program = $kodeProgram AND tb_rekening.kode_kegiatan = $kodeKegiatan) AS T1,
-            (SELECT SUM(tb_rekening.triwulan_2) FROM tb_rekening WHERE tb_rekening.kode_instansi = $kodeInstansi AND tb_rekening.kode_program = $kodeProgram AND tb_rekening.kode_kegiatan = $kodeKegiatan) AS T2,
-            (SELECT SUM(tb_rekening.triwulan_3) FROM tb_rekening WHERE tb_rekening.kode_instansi = $kodeInstansi AND tb_rekening.kode_program = $kodeProgram AND tb_rekening.kode_kegiatan = $kodeKegiatan) AS T3,
-            (SELECT SUM(tb_rekening.triwulan_4) FROM tb_rekening WHERE tb_rekening.kode_instansi = $kodeInstansi AND tb_rekening.kode_program = $kodeProgram AND tb_rekening.kode_kegiatan = $kodeKegiatan) AS T4
-            FROM `tb_kegiatan` WHERE tb_kegiatan.kode_instansi = $kodeInstansi AND tb_kegiatan.kode_program = $kodeProgram")->result();
+        $data_kegiatan = $this->db->query("
+        SELECT tb_kegiatan.kode_kegiatan,tb_kegiatan.nama_kegiatan,tb_kegiatan.total_rinci,
+        (SELECT SUM(tb_rekening.triwulan_1) FROM tb_rekening WHERE tb_rekening.kode_instansi = tb_kegiatan.kode_instansi AND tb_rekening.kode_program = tb_kegiatan.kode_program AND tb_rekening.kode_kegiatan = tb_kegiatan.kode_kegiatan) AS T1,
+        (SELECT SUM(tb_rekening.triwulan_2) FROM tb_rekening WHERE tb_rekening.kode_instansi = tb_kegiatan.kode_instansi AND tb_rekening.kode_program = tb_kegiatan.kode_program AND tb_rekening.kode_kegiatan = tb_kegiatan.kode_kegiatan) AS T2,
+        (SELECT SUM(tb_rekening.triwulan_3) FROM tb_rekening WHERE tb_rekening.kode_instansi = tb_kegiatan.kode_instansi AND tb_rekening.kode_program = tb_kegiatan.kode_program AND tb_rekening.kode_kegiatan = tb_kegiatan.kode_kegiatan) AS T3,
+        (SELECT SUM(tb_rekening.triwulan_4) FROM tb_rekening WHERE tb_rekening.kode_instansi = tb_kegiatan.kode_instansi AND tb_rekening.kode_program = tb_kegiatan.kode_program AND tb_rekening.kode_kegiatan = tb_kegiatan.kode_kegiatan) AS T4
+        FROM `tb_kegiatan` WHERE tb_kegiatan.kode_instansi = $kodeInstansi AND tb_kegiatan.kode_program = $kodeProgram")->result();
         foreach ($data_kegiatan as $kegiatan) {
             $pdf->SetFont("Arial","B",12);
             $pdf->Cell(35,5,@$kegiatan->kode_kegiatan,1,0);
@@ -191,16 +192,17 @@ class Export_pdf extends CI_controller
             $pdf->Cell(30,5,@$kegiatan->T3,1,0,"R");
             $pdf->Cell(30,5,@$kegiatan->T4,1,0,"R");
             $pdf->Ln(5);
-            $data_rekening = $this->db->query("SELECT tb_rekening.kode_patokan,tb_rekening.kode_rekening,tb_rekening.uraian_rekening,tb_rekening.total_rinci,
-                SUM(tb_rekening.triwulan_1) AS T1,SUM(tb_rekening.triwulan_2) AS T2,
-                SUM(tb_rekening.triwulan_3) AS T3,SUM(tb_rekening.triwulan_4) AS T4
-                FROM `tb_rekening` 
-                WHERE tb_rekening.kode_instansi = $kodeInstansi AND tb_rekening.kode_program = $kodeProgram AND tb_rekening.kode_kegiatan = $kodeKegiatan
-                GROUP BY tb_rekening.kode_patokan")->result();  
+            $data_rekening = $this->db->query("
+            SELECT tb_rekening.kode_patokan,tb_rekening.kode_rekening,tb_patokan_rekening.nama,SUM(tb_rekening.total_rinci) AS total_rinci,
+            SUM(tb_rekening.triwulan_1) AS T1,SUM(tb_rekening.triwulan_2) AS T2,
+            SUM(tb_rekening.triwulan_3) AS T3,SUM(tb_rekening.triwulan_4) AS T4
+            FROM tb_rekening INNER JOIN tb_patokan_rekening ON tb_patokan_rekening.kode_patokan = tb_rekening.kode_patokan
+            WHERE tb_rekening.kode_instansi = $kodeInstansi AND tb_rekening.kode_program = $kodeProgram AND tb_rekening.kode_kegiatan = $kegiatan->kode_kegiatan
+            GROUP BY tb_rekening.kode_patokan")->result();  
             foreach ($data_rekening as $rekening) {
                 $pdf->SetFont("Arial","",10);
                 $pdf->Cell(35,5,@$rekening->kode_patokan."  |  ".@$rekening->kode_rekening,1,0);
-                $pdf->Cell(85,5,@$rekening->uraian_rekening,1,0);
+                $pdf->Cell(85,5,@$rekening->nama,1,0);
                 $pdf->Cell(40,5,@$rekening->total_rinci,1,0,"R");
                 $pdf->Cell(30,5,@$rekening->T1,1,0,"R");
                 $pdf->Cell(30,5,@$rekening->T2,1,0,"R");
@@ -209,16 +211,16 @@ class Export_pdf extends CI_controller
                 $pdf->Ln(5);
             }
         }
-        $pdf->SetX(220);
-        $pdf->Cell(30,10,"Sidoarjo, ".date("d F Y"),0,1);
+        $pdf->SetX(212);
+        $pdf->Cell(60,10,"Sidoarjo, ".date("d F Y"),0,1,"C");
         $pdf->SetX(222);
-        $pdf->Cell(40,0,"Nama Sekolah",1,1,"C");
+        $pdf->Cell(40,0,@$data_sekolah[0]->nama_instansi,0,1,"C");
         $pdf->Ln(20);
         $pdf->SetX(222);
-        $pdf->Cell(40,0,"Nama Siswa",1,1,"C");
+        $pdf->Cell(40,0,@$data_siswa[0]->nama,0,1,"C");
         $pdf->Ln(5);
         $pdf->SetX(222);
-        $pdf->Cell(40,0,"Nis",1,1,"C");
+        $pdf->Cell(40,0,@$data_siswa[0]->nis,0,1,"C");
 
         $pdf->Output();
     }
